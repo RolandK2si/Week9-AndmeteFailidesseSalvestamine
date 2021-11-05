15064397cdab4d29cd737cd955438bf8211eb847
Roland Käsi <rkasi@tktk.ee>
2021-11-05 13:02:23 +0200

    add new projects

---
+++ ShoppingListAdd/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingListAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\Samples\shoppinglist";
+            string fileName = @"\\shoppinglist.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> shoppinglist = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add an item Y/N?");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your item:");
+                    string userItem = Console.ReadLine();
+                    shoppinglist.Add(userItem);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+
+            }
+            
+            Console.Clear();
+
+            foreach (string item in shoppinglist)
+            {
+                Console.WriteLine($"Your wish: {item}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", shoppinglist);
+            
+        }
+    }
+}
---
+++ WishList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WishList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> mywishlist = new List<string>();
+
+            Console.WriteLine(mywishlist.Count);
+
+            mywishlist.Add("coffe machine");
+            mywishlist.Add("New pair of socks");
+            mywishlist.Add("Box of chocolates");
+            mywishlist.Add("New pillow");
+
+            Console.WriteLine(mywishlist.Count);
+
+            foreach(string wish in mywishlist)
+            {
+                Console.WriteLine(wish);
+            }
+        }
+    }
+}
